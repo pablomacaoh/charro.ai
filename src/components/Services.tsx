12eb1123eb5a9eefe5d3d83380a29a1cb565d7ff
pablomacaoh <pabcastro@gmail.com>
2025-03-04 20:02:11 -0600

    Update Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,5 +1,5 @@
 
-import { Monitor, LineChart, Database, Wrench, Globe, Zap } from 'lucide-react';
+import { Monitor, LineChart, Database, Wrench, Globe, Zap, message-circle } from 'lucide-react';
 
 const Services = () => {
   const services = [
@@ -14,7 +14,7 @@ const Services = () => {
       description: "Conectamos y personalizamos tus sistemas para maximizar la eficiencia de tu operacion."
     },
     {
-      icon: <Database className="h-6 w-6" />,
+      icon: <message-circle className="h-6 w-6" />,
       title: "Comunicación empresarial vía WhatsApp",
       description: "Fortalecemos la interacción corporativa implementando soluciones integradas de WhatsApp."
     },
