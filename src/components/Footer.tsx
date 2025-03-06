2f5de8397b4bf2ba4110d40237d2aa5fef793e80
pablomacaoh <pabcastro@gmail.com>
2025-03-05 19:31:40 -0600

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   
@@ -5,17 +6,16 @@ const Footer = () => {
     <footer className="py-10 border-t border-gray-100">
       <div className="container mx-auto px-6 md:px-8">
         <div className="flex flex-col md:flex-row justify-between items-center">
-          <div className="mb-4 md:mb-0 flex items-center">
-            <img src="/charroai_logo_hat.png" alt="Logo" className="h-14 w-14 mr-2" />
-            <div>
+          <div className="mb-4 md:mb-0">
+            <img src="/charroai_logo_hat.png" alt="Logo" className="h-14 w-14" />
               <a href="#home" className="font-display text-xl font-bold tracking-tight text-charro-900">
                 Charro.ai
               </a>
-              <p className="text-sm text-charro-600 mt-1">
-                Servicios y Consultoría digital
-              </p>
-            </div>
+            <p className="text-sm text-charro-600 mt-1">
+              Servicios y Consultoría digital
+            </p>
           </div>
+          
           <div className="text-sm text-charro-600">
             © {currentYear} Charro.ai All rights reserved.
           </div>
@@ -24,6 +24,8 @@ const Footer = () => {
     </footer>
   );
 };
+
+export default Footer;
 
 export default Footer;
 
